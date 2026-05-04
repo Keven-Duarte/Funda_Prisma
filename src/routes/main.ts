7085e428e3ebe14a9c1f9a2042b8166b9b1b89cc
Keven-Duarte <Kevenoliveiraduarte2020@gmail.com>
2026-05-04 08:36:22 -0300

    modificando criação

--- src/routes/main.ts
+++ src/routes/main.ts
@@ -4,15 +4,18 @@ import { prisma } from '../libs/prisma';
 export const mainRouter = Router();
 
 mainRouter.get('/ping', (req, res) => {
-res.json({ pong: true });
+    res.json({ pong: true });
 });
 
-mainRouter.post ('/user', async (req, res) => {
+mainRouter.post('/user', async (req, res) => {
 
-const user = await createUser(
+    const user = await prisma.user.create({
+        data: {
+            name: 'John Doe',
+            email: 'john.doe@example.com'
+        }
+    });
 
-'John Doe',
-'john.doe@example.com'
+    res.json(user);
+});
 
-)
-res.json(user) })
